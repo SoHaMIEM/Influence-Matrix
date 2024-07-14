3759dc25701fa855b1c94eedd6d70537e2974a45
SohamIEM <sohghosh2003@gmail.com>
2024-07-15 01:43:11 +0530

    Day 2

--- platform/routes.py
+++ platform/routes.py
@@ -1,6 +1,9 @@
-from flask import render_template
+from flask import Flask, send_from_directory, render_template
+import os
 from app import app
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
