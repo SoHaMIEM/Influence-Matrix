f644089ad0931eefc5337cb225acf661472aae80
SohamIEM <sohghosh2003@gmail.com>
2024-07-15 00:03:03 +0530

    Day 2

--- platform/app.py
+++ platform/app.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-db = SQLAlchemy(app)
+import config
+
+
+
+import routes
+
+
+"""
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'index'
+"""
 
-from controllers import main as main_blueprint
-app.register_blueprint(main_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- platform/models.py
+++ platform/models.py
@@ -1,4 +1,8 @@
-from app import db
+
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
